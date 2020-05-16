70a83a47c1c4f6f6e8081db1280e8afa118a1962
Maxime Le Breton <mail@maximelebreton.com>
2020-05-16 15:08:51 +0200

    fix cors bis

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 var express = require("express");
-var path = require("path");
+//var path = require("path");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server, { origins: "*:*" });
-var cors = require("cors");
+//var cors = require("cors");
 
 // app.get("/", function(req, res) {
 //   res.send("index.html");
 // });
-app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(cors());
 
-app.use(express.static("public"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(express.static("public"));
 
 server.listen(3000, function() {
   //server.listen(process.env.PORT || 3000, function() {
