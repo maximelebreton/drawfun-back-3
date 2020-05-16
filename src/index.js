62e6d5bd3645bf7c8eacbefd5fb199d624edb9be
Maxime Le Breton <mail@maximelebreton.com>
2020-05-16 14:54:56 +0200

    add cors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ var path = require("path");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var cors = require("cors");
 
 // app.get("/", function(req, res) {
 //   res.send("index.html");
 // });
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.static("public"));
