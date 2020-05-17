bf6f75a4f07bf7549ce2f52cb6b2e4525fe8701d
Maxime Le Breton <mail@maximelebreton.com>
2020-05-17 02:42:33 +0200

    ok test

--- src/index.js
+++ src/index.js
@@ -16,19 +16,25 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static("public"));
 //process.env.PORT ||
 
+server.listen(process.env.PORT || 3000, function() {
+  console.log("listening on *:" + (process.env.PORT || 3000));
+});
+
 io.on("connection", function(socket) {
-  console.log(socket, "socket");
+  let id = socket.id;
+  console.log(socket.request.headers.referer, "socket");
   socket.on("toggleRmoteMuteAudio", function(data) {
     io.emit("toggleRmoteMuteAudio", data);
   });
   socket.on("connect", function() {
     console.log("connection established");
   });
+  socket.on("onpan", data => {
+    console.log("onpan");
+    data.id = id;
+    socket.broadcast.emit("onpan", data);
+  });
   socket.on("disconnect", function() {
     console.log("connection closed");
   });
-});
-
-server.listen(process.env.PORT || 3000, function() {
-  console.log("listening on *:" + process.env.PORT || 3000);
 });
